01ff0038cb15fd6aa53451fb97c9ec50763a3954
apoorvkh <apoorv.khand@gmail.com>
2021-10-27 03:15:20 -0700

    Flag to disable data augmentation

--- models/train/train_seq2seq.py
+++ models/train/train_seq2seq.py
@@ -30,6 +30,7 @@ if __name__ == '__main__':
     parser.add_argument('--gpu', help='use gpu', action='store_true')
     parser.add_argument('--dout', help='where to save model', default='exp/model:{model}')
     parser.add_argument('--resume', help='load a checkpoint')
+    parser.add_argument('--no_augmentation', help='disable color swap data augmentation', action='store_true')
 
     # hyper parameters
     parser.add_argument('--batch', help='batch size', default=4, type=int)
